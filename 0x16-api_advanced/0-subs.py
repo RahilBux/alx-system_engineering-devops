040088f9e167fc6eff076e6ca5e85c86be8b23b2
RahilBux <rahilbux@gmail.com>
2024-08-14 02:47:07 +0200

    task 0 fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,12 +4,14 @@ Contains the number_of_subscribers function to use
 """
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """returns the num of subscribers for given"""
-    if subreddit is None or type(subreddit) is not str:
+    """Returns the number of subscribers of a given subreddit user"""
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
         return 0
-    req = requests.get('https://www.reddit.com/r/{}/about.json',format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subs = req.get("data", {}).get("subscribers", 0)
-    return subs
+
+    return sub_info.json().get("data").get("subscribers")
