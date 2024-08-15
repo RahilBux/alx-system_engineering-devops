805f5497fe0493f884238711fb8225ccf2d493aa
RahilBux <rahilbux@gmail.com>
2024-08-15 02:12:50 +0200

    task 0g

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Creates manifest that fix all termination of php
+
+exec { 'fix_phpp':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
+}
