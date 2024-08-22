0d1264a5bdf4a6a49c0d644afd0e83f4dba0cf17
RahilBux <rahilbux@gmail.com>
2024-08-22 22:21:25 +0200

    task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# Increases the traffic an nginx server can handle
+
+# Increase the ulimit of the default
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
